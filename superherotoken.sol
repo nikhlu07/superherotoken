690d4624bbc9ac9e7fcb6a6712026295a8b85ed6
nikhlu07 <100586555+nikhlu07@users.noreply.github.com>
2023-07-22 05:27:00 +0530

    Create superherotoken.sol

---
+++ superherotoken.sol
@@ -0,0 +1,83 @@
+pragma solidity 0.8.18;
+
+contract SuperHeroToken {
+    string public constant name = "SuperHeroToken";
+    string public constant symbol = "HERO";
+    uint8 public constant decimals = 18;
+    uint public totalSupply;
+
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowances;
+    mapping(uint256 => address) public tokenIdToOwner;
+    mapping(address => uint256[]) public ownedTokens;
+    mapping(uint256 => bool) public tokenExists;
+
+    uint256 public nextTokenId = 1;
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed spender, uint256 indexed tokenId);
+    event Mint(address indexed to, uint256 indexed tokenId);
+    event Burn(address indexed from, uint256 indexed tokenId);
+
+    constructor(uint256 initialSupply) {
+        totalSupply = initialSupply * (10**uint256(decimals));
+        balances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 tokenId) external returns (bool) {
+        require(to != address(0), "Invalid recipient address");
+        require(tokenExists[tokenId], "Token does not exist");
+        require(tokenIdToOwner[tokenId] == msg.sender, "You don't own this token");
+
+        tokenIdToOwner[tokenId] = to;
+        emit Transfer(msg.sender, to, tokenId);
+        return true;
+    }
+
+    function approve(address spender, uint256 tokenId) external returns (bool) {
+        require(tokenExists[tokenId], "Token does not exist");
+        require(tokenIdToOwner[tokenId] == msg.sender, "You don't own this token");
+
+        allowances[msg.sender][spender] = tokenId;
+        emit Approval(msg.sender, spender, tokenId);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) external returns (bool) {
+        require(to != address(0), "Invalid recipient address");
+        require(tokenExists[tokenId], "Token does not exist");
+        require(tokenIdToOwner[tokenId] == from, "Token is not owned by sender");
+        require(allowances[from][msg.sender] == tokenId, "Not approved to transfer this token");
+
+        tokenIdToOwner[tokenId] = to;
+        delete allowances[from][msg.sender];
+        emit Transfer(from, to, tokenId);
+        return true;
+    }
+
+    function mint(address to) external {
+        require(nextTokenId <= totalSupply, "Maximum token limit reached");
+
+        uint256 tokenId = nextTokenId;
+        tokenIdToOwner[tokenId] = to;
+        ownedTokens[to].push(tokenId);
+        tokenExists[tokenId] = true;
+        nextTokenId++;
+
+        emit Mint(to, tokenId);
+        emit Transfer(address(0), to, tokenId);
+    }
+     function burn(uint256 tokenId) external {
+        require(tokenExists[tokenId], "Token does not exist");
+        require(tokenIdToOwner[tokenId] == msg.sender, "You don't own this token");
+        require(balances[msg.sender] >= 1, "Insufficient balance for burning");
+
+        tokenIdToOwner[tokenId] = address(0);
+        tokenExists[tokenId] = false;
+        balances[msg.sender]--;
+
+        emit Burn(msg.sender, tokenId);
+        emit Transfer(msg.sender, address(0), tokenId);
+    }
+
+}
